2025a8c193caa388f04a80ebf97d2baada3ecc80
Maninder Singh <maninder.singh@outlook.in>
2026-01-11 21:54:59 -0500

    Update version of Milow Terminal to 0.0.2.4

--- Casks/milow-terminal.rb
+++ Casks/milow-terminal.rb
@@ -1,5 +1,5 @@
 cask "milow-terminal" do
-  version "0.0.2"
+  version "0.0.2.4"
   sha256 "d53b99035ae1c94f240dbbbbc299f0cb0a04126301d72f4a2a3597040a2b84ee"
   url "https://github.com/Maninder-mike/milow/releases/download/beta/MilowTerminal-macos-beta.zip"
   name "Milow Terminal"
